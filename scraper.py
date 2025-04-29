d756e69fb265804a62c83de2d42311bcf0033ddf
AnthonyCoding373 <ashkinaa@uci.edu>
2025-04-29 14:07:03 +0000

    fixed typo

--- scraper.py
+++ scraper.py
@@ -34,7 +34,6 @@ def extract_next_links(url, resp):
 
             if href and not href.startswith("#"):
                 found_urls.add(href)
-    else if repo.status
 
     return list(found_urls)
 
